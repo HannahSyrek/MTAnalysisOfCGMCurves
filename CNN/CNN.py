b64b074335ae27a6707ecdedf298f4a50b54a383
HannahSyrek <HSyrek@gmx.de>
2018-03-10 10:09:34 +0100

    added CNN.py

---
+++ CNN/CNN.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: hannah syrek
+
+This script implements a convolutional neural network to classify the data 
+of diabetes type 1 patients.
+"""
+
+# Imports
+import numpy as np
+import os
+from utilities import *
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+
+
+
---
+++ CNN/utilities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  9 11:13:38 2018
+
+@author: hannah syrek
+"""
+
+
+import pandas as pd 
+import numpy as np
+import os
+
+def read_data():
+    return d
+
+
